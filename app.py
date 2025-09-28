066f289c16bbb36deaf372fc024fec2de4878cb4
Arafath J <arafathj424@gmail.com>
2025-09-28 15:39:17 +0530

    Created a database model

--- app.py
+++ app.py
@@ -3,9 +3,23 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///projetdb.db"
+db = SQLAlchemy(app)
+
+# creating model that represent a unit of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String[100], nullable = False )
+    complete = db.Column(db.Integer, default = 0)
+    created = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
 
 
 @app.route('/')
@@ -13,4 +27,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
+
+    with app.app_context():
+        db.create_all()
+        
     app.run(debug = True)
